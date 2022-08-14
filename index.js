80afb761f8b215fa1eb976a7e59659452b487dc8
+ <+>
2022-08-14 12:38:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+function multiply(num1, num2){
+    answer = num1 * num2;
+    return answer
+} multiply(31, 2)
+
+function random(max){
+return Math.floor(Math.random() * max) + 1 ;
+}console.log(random(3) );
+
+function mod(num3, num4){
+    answer = num3 % num4;
+    return answer
+}mod(23, 10)
+
+function max(num1, num2){
+    return Math.max( num1, num2 );
+}console.log(max( 10, 20 ));
