d707bc8bd8de5c7f098c7fadc3a954b130be0699
+ <+>
2022-08-24 21:56:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 
 // Write your code here
-function multiply(num1, num2){
-    answer = num1 * num2;
-    return answer
-} multiply(31, 2)
+const num1 = 31
+const num2 = 2
 
-function random(max){
-return Math.floor(Math.random() * max) + 1 ;
-}console.log(random(3) );
+const multiply = num1*num2
 
-function mod(num3, num4){
-    answer = num3 % num4;
-    return answer
-}mod(23, 10)
 
-function max(num1, num2){
-    return Math.max( num1, num2 );
-}console.log(max( 10, 20 ));
+const random = Math.floor(Math.random() * 10)+1;
+
+const num3 = 24
+const num4 = 5
+const mod =  num3%num4
+
+const numbers = [12,16,20]
+const max = (Math.max(...numbers));
